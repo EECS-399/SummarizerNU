bbb2518d43448629d60ab7f04356207e45ac72f1
David MacCumber <davidmaccumber2021@u.northwestern.edu>
2018-10-09 19:31:39 -0500

    Removed API keys and created uploader script

---
+++ uploader.py
@@ -0,0 +1,12 @@
+import json
+import tweepy
+import csv
+
+with open("twitter_credentials.json", "r") as file:
+    creds = json.load(file)
+
+auth = tweepy.OAuthHandler(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+auth.set_access_token(creds['ACCESS_TOKEN'], creds['ACCESS_SECRET'])
+
+api = tweepy.API(auth)
+api.update_status("This is an automated Tweet created in Python and called from Terminal")
